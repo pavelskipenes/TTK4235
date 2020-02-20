5c79cf606b0330489e104d2e12e30e3160b0cce8
Sindre <sindrbov@stud.ntnu.no>
2020-02-20 19:54:28 +0100

    Minor comment changes

--- elevator/source/sensor.h
+++ elevator/source/sensor.h
@@ -1,7 +1,7 @@
 #ifndef ELEVATOR_SENSOR_H
 #define ELEVATOR_SENSOR_H
 
-// <insert>ModeReader leser alle sensorere relevante til den tilstanden, og oppdaterer programvariablene.
+// <insert> ModeReader leser alle sensorere relevante til den tilstanden, og oppdaterer programvariablene.
 
 /**
  * @file
