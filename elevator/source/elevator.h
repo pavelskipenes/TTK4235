0fdfe9dda3341c52a5be071da57e7936564d8fa0
Sindre <sindrbov@stud.ntnu.no>
2020-02-20 19:10:59 +0100

    Added doxygen documentation for elevator.h

--- elevator/source/elevator.h
+++ elevator/source/elevator.h
@@ -1,3 +1,9 @@
+/**
+ * @file
+ * @brief Library containing program variables, and the main program loop @c Elevator()
+ * 
+ */
+
 #ifndef ELEVATOR_H
 #define ELEVATOR_H
 #include <stdbool.h>
@@ -8,25 +14,79 @@
 
 #define DOOR_OPEN_TIME 3
 
-static bool upOrders[HARDWARE_NUMBER_OF_FLOORS] = {false};
-static bool downOrders[HARDWARE_NUMBER_OF_FLOORS] = {false};
-static bool insideOrders[HARDWARE_NUMBER_OF_FLOORS] = {false};
-static bool stopButtonPressed = false;
-static bool hasOrders = false;
-static bool doorTimerStarted = false;
-static bool obstruction = false;
-static bool atSomeFloor = false;
-static bool floorSensor[HARDWARE_NUMBER_OF_FLOORS] = {false};
-static int lastKnownFloor;
-static unsigned int Iterations = 0;
 
-// Elevator direction
+
+/**
+ * @brief Static boolean array, where index 0-3 represents floor 1-4, and value @c true represents an order in direction @c UP at that floor.
+ * 
+ */
+static bool upOrders[HARDWARE_NUMBER_OF_FLOORS] = {false};
+/**
+ * @brief Static boolean array, where index 0-3 represents floor 1-4, and value @c true represents an order in direction @c DOWN at that floor.
+ * 
+ */
+static bool downOrders[HARDWARE_NUMBER_OF_FLOORS] = {false};
+/**
+ * @brief Static boolean array, where index 0-3 represents floor 1-4, and value @c true represents an order for that floor coming from inside the elevator.
+ * 
+ */
+static bool insideOrders[HARDWARE_NUMBER_OF_FLOORS] = {false};
+/**
+ * @brief Boolean variable telling whether stop button input is high or not.
+ * 
+ */
+static bool stopButtonPressed = false;
+/**
+ * @brief Boolean variable telling whether there are any unfulfilled orders or not.
+ * 
+ */
+static bool hasOrders = false;
+/**
+ * @brief Boolean variable telling whether obstruction input is high or not.
+ * 
+ */
+static bool obstruction = false;
+/**
+ * @brief Boolean variable telling whether the elevator is currently at a known floor. @c false if elevator is between floors.
+ * 
+ */
+static bool atSomeFloor = false;
+/**
+ * @brief The previous floor the elevator was known to be at.
+ * 
+ */
+static int lastKnownFloor;
+
+
+/**
+ * @brief The different movement directions the elevator can have.
+ * 
+ */
 typedef enum {
     UP,
     NONE,
     DOWN
 } Direction;
 
+/**
+ * @brief Global variable; current elevator direction.
+ * 
+ */
 Direction direction;
+
+/**
+ * @brief Main program loop. Call to initiate and run elevator control logic.
+ * 
+ * @code
+ * //Example:
+ * 
+ * #include <elevator.h>
+ * 
+ * int main(){
+ *     Elevator();
+ * }
+ * @endcode
+ * 
+ */
 void Elevator();
 #endif
