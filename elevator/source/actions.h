830e74cb02bc27f041708f91fe66f235e0d51cc9
Sindre <sindrbov@stud.ntnu.no>
2020-02-20 19:16:11 +0100

    Corrected various spelling errors, added comments for future changes.

--- elevator/source/actions.h
+++ elevator/source/actions.h
@@ -1,9 +1,15 @@
 #ifndef ELEVATOR_ACTIONS_H
 #define ELEVATOR_ACTIONS_H
 
+/**
+ * @file
+ * @brief 
+ * 
+ */
+
 void elevatorMove(Direction);
 void elevatorMove();
-void elevatorStop();
+void elevatorStop();    // rename to stopElevator
 void elevatorMoveTo(int); // Used only by insideOrders
 void clearAllOrders();
 void clearOrdersAtThisFloor();
--- elevator/source/modes.h
+++ elevator/source/modes.h
@@ -1,6 +1,12 @@
 #ifndef ELEVATOR_MODES_H
 #define ELEVATOR_MODES_H
 
+/**
+ * @file
+ * @brief 
+ * 
+ */
+
 void init();
 void running();
 void idle();
