f578930b0a4661b4fc99d6e2cdb0eb7eaefbe88e
Pavel Skipenes <pavelgs@stud.ntnu.no>
2020-04-27 10:49:52 +0200

    started 1_io

--- microBit/not_butter/1_io/source/button.c
+++ microBit/not_butter/1_io/source/button.c
@@ -2,18 +2,20 @@
 #include "gpio.h"
 
 /* Fyll inn disse to */
-#define BUTTON_A_PIN ??
-#define BUTTON_B_PIN ??
+#define BUTTON_A_PIN 17
+#define BUTTON_B_PIN 26
+#define OUTPUT 0
+#define INPUT 1
 
 void button_init(){
-    GPIO->PIN_CNF[BUTTON_A_PIN] = 0;
-    GPIO->PIN_CNF[BUTTON_B_PIN] = 0;
+    GPIO->PIN_CNF[BUTTON_A_PIN] = OUTPUT;
+    GPIO->PIN_CNF[BUTTON_B_PIN] = OUTPUT;
 }
 
 uint8_t button_a_pressed(){
-    /* Fill me up with hot code */
+    return ((GPIO->IN) & (1 << BUTTON_A_PIN));
 }
 
 uint8_t button_b_pressed(){
-    /* Fill me up with hot code */
+        return ((GPIO->IN) & (1 << BUTTON_B_PIN));
 }
