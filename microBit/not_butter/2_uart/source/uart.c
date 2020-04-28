12eee5057d7e33529be6fe6f964c39746f5c3061
Pavel Skipenes <pavelgs@stud.ntnu.no>
2020-04-28 17:48:30 +0200

    Completed 2nd assignment: UART

--- microBit/not_butter/2_uart/source/uart.c
+++ microBit/not_butter/2_uart/source/uart.c
@@ -1,23 +1,43 @@
 #include "uart.h"
 #include "gpio.h"
 
-/* Fyll inn disse */
-#define PIN_TX ??
-#define PIN_RX ??
-#define PIN_DISABLE 0x????????
-#define BAUD_9600 0x????????
+#define PIN_TX 24
+#define PIN_RX 25
+#define PIN_DISABLE 0xFFFFFFFF
+
+#define BAUD_1200 0x0004F000
+#define BAUD_2400 0x0009D000
+#define BAUD_4900 0x0013B000
+#define BAUD_9600 0x00275000
+#define BAUD_14400 0x003B0000
+#define BAUD_19200 0x004EA000
+#define BAUD_28800 0x0075F000
+#define BAUD_48400 0x009D5000
+#define BAUD_57600 0x00EBF000
+#define BAUD_76800 0x013A9000
+#define BAUD_115200 0x01D7E000
+#define BAUD_230400 0x03AFB000
+#define BAUD_250000 0x04000000
+#define BAUD_460800 0x075F7000
+#define BAUD_921600 0x0EBEDFA4
+#define BAUD_1M 0x10000000
 
 #define UART_DISABLE 0
 #define UART_ENABLE 4
 
-/* Fyll inn riktig startadresse */
-#define UART ((NRF_UART_REG*)0x????????)
+#define UART ((NRF_UART_REG*)0x40002000)
+
+#define INPUT 0
+#define OUTPUT 1
 
 typedef struct {
     /* Tasks */
-    /* Her skal det fylles inn seks felter */
-
-
+	volatile uint32_t STARTRX;
+	volatile uint32_t STOPRX;
+	volatile uint32_t STARTTX;
+	volatile uint32_t STOPTX;
+	volatile uint32_t RESERVED0[3];
+	volatile uint32_t SUSPEND;
 	/* Events */
 	volatile uint32_t RESERVED1[56];
 	volatile uint32_t CTS;
@@ -54,12 +74,15 @@ typedef struct {
 void uart_init(){
     UART->ENABLE = UART_DISABLE;
 
-    GPIO->PIN_CNF[PIN_TX] = 1;
-    GPIO->PIN_CNF[PIN_RX] = 0;
+    GPIO->PIN_CNF[PIN_TX] = OUTPUT;
+    GPIO->PIN_CNF[PIN_RX] = INPUT;
+	
+	UART->PSELTXD = PIN_TX;
+	UART->PSELRXD = PIN_RX;
+	UART->PSELRTS = PIN_DISABLE;
+	UART->PSELCTS = PIN_DISABLE;
 
-    /* Konfigurer "Pin Select" for RTS, CTS, TXD, og RXD */
-
-    /* Konfigurer Baudhastigheten */
+	UART->BAUDRATE = BAUD_9600;
 
     UART->ENABLE = UART_ENABLE;
 
@@ -67,10 +90,18 @@ void uart_init(){
 }
 
 void uart_send(uint8_t byte){
-    /* Fyll inn dette */
+	// Section 29.4
+	UART->STARTTX = 1;
+	UART->TXD = byte;
+	
+	while(!UART->TXDRDY);
+	UART->TXDRDY = 0;
+
+	UART->STOPTX = 1;
 }
 
 uint8_t uart_receive(uint8_t * p_byte){
+	// Section 29.5
     if(!UART->RXDRDY)
         return 0;
 
